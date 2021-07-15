6c097f612833b9db0839c2dfbb0c2bb0acf51e9b
Misael Lopes <mecl.ely@gmail.com>
2021-07-15 16:48:10 +0100

    2/10 tests done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
-
+const users = [];
+ 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find( (user) => user.username === username )
+
+  if (!user) {
+    return response.status(404).json({ error: 'User Account Not Found!'})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+ 
+  const { name, username } = request.body
+  const userAlreadyExists = users.some( (users) => users.username = username )
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: 'Username Is Already Taken!'})
+  }
+
+  user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user)
+  
+  return response.status(201).json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
