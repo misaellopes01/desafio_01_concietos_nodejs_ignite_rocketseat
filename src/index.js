2f29762c21285f1b3aca6962707d5d72609e45f3
Misael Lopes <mecl.ely@gmail.com>
2021-07-15 19:11:11 +0100

    Done, but only 3/10 test

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.post('/users', (request, response) => {
     return response.status(400).json({ error: 'Username Is Already Taken!'})
   }
 
-  user = {
+  const user = {
     id: uuidv4(),
     name,
     username,
@@ -46,23 +46,76 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body
+  const { user } = request
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo)
+
+  return response.status(201).json(todo)
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
+  const { title, deadline } = request.body
+  const { id } = request.params
+  const { user } = request
+
+  const todo = user.todos.find( (todo) => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo Does Not Exists!'})
+  }
+
+  todo.title = title
+  todo.deadline = new Date(deadline)
+  
+  return response.status(200).json(todo)
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params
+  const { user } = request
+
+  const todo = user.todos.find( (todo) => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo Does Not Exists!'})
+  }
+
+  todo.done = true
+
+  return response.status(200).send()
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params
+  const { user } = request
+
+  const todo = user.todos.find( (todo) => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo Does Not Exists!'})
+  }
+
+  user.todos.splice(todo, 1)
+
+  return response.status(200).json(user.todos)
 });
 
 module.exports = app;
